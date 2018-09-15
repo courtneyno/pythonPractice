cbca9965aa233a6e6137dc2dc81e691a91a74f3b
Bickio O'Callahan <boca359@aucklanduni.ac.nz>
2018-09-15 15:35:49 +1200

    remove comments in task2

--- task2.py
+++ task2.py
@@ -6,28 +6,22 @@ Runs a find the number game
 
 import random
 
-# number is chosen
+
 answer = random.randint(1, 100)
 
 while True:
-
-    # number is guessed
     guess = input('Enter your number: ')
     guess = int(guess)
 
-    # either correct or number is higher or lower
     is_correct = answer == guess
 
-    # if number is correct game is finished: tell them they won, and exit
     if is_correct:
         print('you won')
         exit()
 
-    # if number is wrong then say if higher or lower
     is_higher = answer < guess
 
     if is_higher:
         print('guess lower')
-
     else: 
         print('guess higher')
